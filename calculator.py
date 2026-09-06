5b78ab75932272c715f3365e989172fed9208e9e
elam29042006-ctrl <elam29042006@example.com>
2026-09-06 18:29:34 +0530

    elamparithita2024-cloud

---
+++ calculator.py
@@ -0,0 +1,10 @@
+# Python Calculator
+
+a = 20
+b = 5
+
+print("Calculator Output")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
